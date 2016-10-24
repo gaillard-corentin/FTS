2be43eada3cb6822e7f8225d129f942e0a8d730e
TheophaneJoseph <theophane.joseph@epita.fr>
2016-10-24 23:57:35 +0200

    Déplacement
    Avancer
    Reculer
    Tourner

---
+++ TransformFunctions.cs
@@ -0,0 +1,37 @@
+﻿
+	// Use this for initialization
+	using UnityEngine;
+	using System.Collections;
+	
+	public class TransformFunctions : MonoBehaviour
+	{
+		public float moveSpeed = 10f;
+		public float jumpspeed = 15f;
+		public float turnSpeed = 50f;
+		
+		
+		void Update ()
+		{
+			
+			float h = Input.GetAxis ("Vertical"); 
+			
+			if(Input.GetKey(KeyCode.UpArrow))
+				transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+			
+			if(Input.GetKey(KeyCode.DownArrow))
+				transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+			
+			if(Input.GetKey(KeyCode.LeftArrow))
+				transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+			
+			if(Input.GetKey(KeyCode.RightArrow))
+				transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+		  	/*if (Input.GetKey (KeyCode.Space))
+				transform.Translate (Vector3.up * jumpspeed * Time.deltaTime); */
+				
+				
+				
+
+		}
+	}	
+	
