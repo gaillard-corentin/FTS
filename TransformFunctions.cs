7e6019611367ab2d2f88c151927a0ff2aad97333
TheophaneJoseph <theophane.joseph@epita.fr>
2016-10-25 22:51:44 +0200

    VUE FPS + DEPLACEMENT

--- TransformFunctions.cs
+++ TransformFunctions.cs
@@ -6,28 +6,66 @@
 	public class TransformFunctions : MonoBehaviour
 	{
 		public float moveSpeed = 10f;
-		public float jumpspeed = 15f;
+		
 		public float turnSpeed = 50f;
 		
-		
+		public float sensiX =20f;
+		public float sensiY = 20f;
+		public float minX = -180f;
+		public float maxX = 180f;
+		public float minY = -180f;
+		public float maxY = 180f;
+
+
+		float rotationX = 0f;
+		float rotationY = 0f;
+
+
+
+		Quaternion rotationplayer;
+		Quaternion rotationcamera;
+
+		public Camera cam  ;
+		CharacterController charac ;
+
+		Vector3 mov = Vector3.zero;
+
+		void Start()
+		{
+		charac = gameObject.GetComponent<CharacterController>();
+		rotationplayer = transform.localRotation;
+		rotationcamera = cam.transform.localRotation;
+
+
+
+		}
+
 		void Update ()
 		{
+			//Rotationcamera
+			rotationX = Input.GetAxis ("MouseX") * sensiX;
+			rotationY = Input.GetAxis ("MouseY") * sensiY;
+			rotationX = Mathf.Clamp (rotationX, minX, maxX);
+			rotationY = Mathf.Clamp (rotationY, minY, maxY);
+			Quaternion xrot = Quaternion.AngleAxis (rotationX, Vector3.up);
+			Quaternion yrot = Quaternion.AngleAxis (rotationY, Vector3.right);
+			transform.localRotation = rotationplayer * xrot;
+			cam.transform.localRotation = rotationcamera * yrot;
 			
-			float h = Input.GetAxis ("Vertical"); 
-			
-			if(Input.GetKey(KeyCode.UpArrow))
+			//Deplacement
+			if(Input.GetKey(KeyCode.Z))
 				transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
 			
-			if(Input.GetKey(KeyCode.DownArrow))
+			if(Input.GetKey(KeyCode.S))
 				transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
 			
-			if(Input.GetKey(KeyCode.LeftArrow))
-				transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+			if(Input.GetKey(KeyCode.Q))
+				
+				transform.Translate (-Vector3.right * moveSpeed * Time.deltaTime);
 			
-			if(Input.GetKey(KeyCode.RightArrow))
-				transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
-		  	/*if (Input.GetKey (KeyCode.Space))
-				transform.Translate (Vector3.up * jumpspeed * Time.deltaTime); */
+			if (Input.GetKey (KeyCode.D))
+				transform.Translate (Vector3.right * moveSpeed * Time.deltaTime);
+		  	
 				
 				
 				
