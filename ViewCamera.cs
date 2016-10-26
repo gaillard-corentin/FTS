4c334131487238c0b6866f517184d5d203891985
TheophaneJoseph <theophane.joseph@epita.fr>
2016-10-26 17:30:55 +0200

    CHANGEMENT DE VUE (TPS par défaut)

---
+++ ViewCamera.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViewCamera : MonoBehaviour {
+
+	public Camera Maincamera;
+	public Camera TPScamera;
+	bool switchcam = false;
+
+	// Use this for initialization
+	void Start () {
+	
+		Maincamera.camera.enabled= false;
+		TPScamera.camera.enabled = true;
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown ("e")) {
+			switchcam = !switchcam;
+								
+
+			if (switchcam) {
+				Maincamera.camera.enabled = true;
+				TPScamera.camera.enabled = false;
+			} else {
+				Maincamera.camera.enabled = false;
+				TPScamera.camera.enabled = true;
+
+
+			}
+
+		}
+
+
+	}
+}
