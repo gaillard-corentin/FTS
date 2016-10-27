533e0147f73765cd51dfb489cbee7e58b524d737
TheophaneJoseph <theophane.joseph@epita.fr>
2016-10-27 22:06:45 +0200

    TIRER + RECHARGER
    Arme attaché au MainCamera (FPS)
    Affichage des munitions

--- Shoot.cs
+++ Shoot.cs
@@ -7,23 +7,42 @@ public class Shoot : MonoBehaviour {
 	public int bulletspeed = 25;
 	public float cadence = 0.5f;
 	float cooldown = 0f;
-	bool mode = false;
+	public int munition = 15;
+	public int maxmun = 15;
 
 
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButton ("Fire1") && Time.time > cooldown) {
-			cooldown = Time.time + cadence;
+
+			if (munition >0)
+
+			{
+				cooldown = Time.time + cadence;
 		
 	
-			Rigidbody bullet;
+				Rigidbody bullet;
 
-			bullet = Instantiate (bulletcasing, transform.position, transform.rotation) as Rigidbody;
+				bullet = Instantiate (bulletcasing, transform.position, transform.rotation) as Rigidbody;
+				munition -= 1;
+				bullet.velocity = transform.TransformDirection (Vector3.forward * bulletspeed);
+			
+			}
 
-			bullet.velocity = transform.TransformDirection (Vector3.forward * bulletspeed);
+
 		}
 
 
+		if (Input.GetKeyDown ("r")) {
+
+			munition += maxmun - munition;
+		}
+
+	}
+
+
+	void OnGUI() {
+		GUI.Box (new Rect (10, 10, 130, 25), "Munition : " + munition);
 	}
 }
