02c02cbcc63ebef4b27e4cd2cf476fdb5fef20c6
TheophaneJoseph <theophane.joseph@epita.fr>
2016-10-27 21:21:45 +0200

    Suppression des balles tirés après 3 sec

---
+++ BulletNone.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletNone : MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds (3);
+		Destroy (gameObject);
+	}
+	
+
+}
