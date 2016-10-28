6d113767c65d365546181445d56f1d0f46b0520f
TheophaneJoseph <theophane.joseph@epita.fr>
2016-10-28 11:28:55 +0200

    Faire des dégâts fixe sur un ennemi

---
+++ EnnemyDamage.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnnemyDamage : MonoBehaviour {
+
+	public int ennemyHealth = 100;
+
+
+	void OnCollisionEnter(Collision col) {
+		if (col.gameObject.tag == "Bullet") {
+			ennemyHealth -= 10;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (ennemyHealth <= 0) {
+			Destroy (gameObject);
+		}
+	}
+}
