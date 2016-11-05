aa51f78bbb63bf6626785485481decce1a500b0e
TheophaneJoseph <theophane.joseph@epita.fr>
2016-11-05 15:19:41 +0100

    SPAWN BATMIENTS

---
+++ BuildCube.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildCube : MonoBehaviour {
+
+	public Rigidbody building ;	
+	public Vector3 spawnpos ;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.L)) {
+
+			Rigidbody build ;
+
+			build = Instantiate(building,transform.position + spawnpos, transform.rotation) as Rigidbody;
+
+
+		}
+	}
+}
