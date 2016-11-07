cf5ebadcb91cfa735d22b1f089dd75c62c9fd5e0
Gundalfe <julien.bellec@epita.fr>
2016-11-07 23:41:13 +0100

    Add files via upload

---
+++ BuildManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildManager : MonoBehaviour {
+    private GameObject turrettobuild;
+
+    public GameObject getTurretTobuild()
+    {
+        return turrettobuild;
+    }
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
