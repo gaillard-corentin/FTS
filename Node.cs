d807ff41e9026a2625ddb4d8a9b51ac67b58e4ff
Gundalfe <julien.bellec@epita.fr>
2016-11-07 23:41:46 +0100

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour
+{
+    private GameObject turret;
+
+    public Color hovercolor;
+    private Renderer rend;
+    private Color Startcolor;
+    
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        Startcolor = rend.material.color;
+        
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Nope");
+            return;
+        }
+    }
+    void OnMouseEnter()
+    {
+        rend.material.color = hovercolor;
+
+    }
+    void OnMouseExit()
+    {
+        rend.material.color = Startcolor;
+    }
+}
